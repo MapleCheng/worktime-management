1cc33caf5539c6709068c4089abd396cd68710fb
MapleCheng <maplecheng1313@gmail.com>
2020-12-31 21:32:03 +0800

    adjust redux

--- src/reducers/student.js
+++ src/reducers/student.js
@@ -1,11 +1,10 @@
 import { SET_STUDENT_LIST } from "../actions/student";
-// import AxiosGet from '../AxiosGet'
 
 const data = {
   student_list: [],
 };
 
-export default (state = data, action) => {
+const Student = (state = data, action) => {
   switch (action.type) {
     case SET_STUDENT_LIST:
       return { student_list: action.payload.student_list };
@@ -13,3 +12,4 @@ export default (state = data, action) => {
       return state;
   }
 };
+export default Student;
