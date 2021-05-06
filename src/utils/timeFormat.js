fe33bf58998cd794deaf8d5edc493e2a8ce7e14e
MapleCheng <maplecheng1313@gmail.com>
2021-05-06 11:55:15 +0800

    adjust time format

--- src/utils/timeFormat.js
+++ src/utils/timeFormat.js
@@ -1,10 +1,16 @@
 export const hourFormat = (time) => {
   time = parseInt(time) || 0;
-  let hour = Math.floor(time / 60);
-  return hour < 10 && hour >= 0 ? `0${hour}` : `${hour}`;
+  if (time>=0) {
+    let hour = Math.floor(time / 60);
+    return hour < 10 && hour >= 0 ? `0${hour}` : `${hour}`;
+  } else {
+    let hour = Math.floor(Math.abs(time) / 60);
+
+    return `- ${hour}`
+  }
 };
 export const minuteFormat = (time) => {
   time = parseInt(time) || 0;
   let minute = time % 60;
-  return minute < 10 && minute >= 0 ? `0${minute}` : `${minute}`;
+  return minute < 10 && minute >= 0 ? `0${minute}` : `${Math.abs(minute)}`;
 };
